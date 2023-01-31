af3402518460ccc71d52422d9e39a42c6849f658
shz-cod <web.shahidul.alam@gmail.com>
2023-01-31 19:03:35 +0600

    1

---
+++ js/app.js
@@ -0,0 +1,86 @@
+let scanBtn = document.getElementById("scan_btn"),
+    stopBtn = document.getElementById("stop_btn");
+
+const quaggaInit = () => {
+    Quagga.init(
+        {
+            inputStream: {
+                type: "LiveStream",
+                constraints: {
+                    width: 640,
+                    height: 360,
+                    facingMode: "environment",
+                },
+            },
+            frequency: 1,
+            locator: {
+                patchSize: "medium",
+                halfSample: true,
+            },
+            numOfWorkers: navigator.hardwareConcurrency
+                ? navigator.hardwareConcurrency
+                : 4,
+            decoder: {
+                readers: [
+                    "code_128_reader",
+                    "ean_reader",
+                    "ean_8_reader",
+                    "code_39_reader",
+                    "code_39_vin_reader",
+                    "codabar_reader",
+                    "upc_reader",
+                    "upc_e_reader",
+                    "i2of5_reader",
+                ],
+                debug: {
+                    showCanvas: true,
+                    showPatches: true,
+                    showFoundPatches: true,
+                    showSkeleton: true,
+                    showLabels: true,
+                    showPatchLabels: true,
+                    showRemainingPatchLabels: true,
+                    boxFromPatches: {
+                        showTransformed: true,
+                        showTransformedBox: true,
+                        showBB: true,
+                    },
+                },
+            },
+        },
+        function (err) {
+            if (err) {
+                document.getElementById("v").innerHTML = err;
+                return;
+            }
+            document.getElementById("v").innerHTML =
+                "Initialization finished. Ready to start";
+            Quagga.start();
+        }
+    );
+};
+
+
+scanBtn.addEventListener("click", () => {
+    let status = scanBtn.getAttribute("data-status");
+    if (status === "false") {
+        document.getElementById("interactive").style.display = "block";
+        quaggaInit();
+        scanBtn.setAttribute("data-status", "true");
+    }
+});
+
+stopBtn.addEventListener("click", () => {
+    let status = scanBtn.getAttribute("data-status");
+    if (status === "true") {
+        Quagga.stop();
+        scanBtn.setAttribute("data-status", "false");
+        document.getElementById("interactive").style.display = "none";
+    }
+});
+
+Quagga.onDetected(function (result) {
+    console.log(result.codeResult.code);
+    document.getElementById("v").innerHTML = `${result.codeResult.code}`;
+    JsBarcode("#barcode", `${result.codeResult.code}`);
+});
